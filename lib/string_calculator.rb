90f256feb2800c2ade951a1d1ac31d26a084b51b
Sachin Joshi <scj50499@gmail.com>
2025-05-28 15:18:46 +0530

    feat(calculator): support newlines as separators

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,20 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    
+    if numbers.start_with?('//')
+      delimiter, numbers = extract_custom_delimiter(numbers)
+      return numbers.split(delimiter).map(&:to_i).sum
+    end
+    
     numbers.split(/,|\n/).map(&:to_i).sum
+  end
+
+  private
+  
+  def extract_custom_delimiter(numbers)
+    delimiter_line, remaining = numbers.split("\n", 2)
+    delimiter = delimiter_line[2..-1] # Skip the '//'
+    [delimiter, remaining]
   end
 end
