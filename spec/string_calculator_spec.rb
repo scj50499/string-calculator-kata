8f6ef8ba8a2bb1dc973429c0c0cb73c7e6c7c79e
Sachin Joshi <scj50499@gmail.com>
2025-05-28 15:17:17 +0530

    feat(calculator): support newlines as separators

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,5 +15,13 @@ describe StringCalculator do
     it 'returns sum for two numbers' do
       expect(calculator.add('1,2')).to eq(3)
     end
+
+    it 'returns sum for multiple numbers' do
+      expect(calculator.add('1,2,3,4')).to eq(10)
+    end
+
+    it 'returns sum for numbers separated by newlines' do
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
